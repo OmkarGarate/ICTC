88ea1fe761e43cb2e296814357d378fbbf5baa48
Omkar Garate <garateomkar143@gmail.com>
2026-05-17 13:26:08 +0530

    added frontend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://project-g8lns.vercel.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
